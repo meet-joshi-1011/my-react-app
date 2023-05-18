aeb0fdf7a8c183dd6ca0ea48326a7dc2d8e5f707
meet joshi <85394474+meet-joshi-1011@users.noreply.github.com>
2023-05-19 00:40:42 +0530

    03-015 is completed

--- src/App.js
+++ src/App.js
@@ -5,25 +5,25 @@ function App() {
       id: 1,
       title: 'Alien Ware',
       amount: 999,
-      date: new Date(2020,5,18) 
+      date: new Date(2020,4,19) 
     },
     {
       id: 2,
       title: 'Asus Rog 9',
       amount: 888,
-      date: new Date(2020,5,18) 
+      date: new Date(2020,5,19) 
     },
     {
       id: 3,
       title: 'Jagwar',
       amount: 999999,
-      date: new Date(2020,5,18) 
+      date: new Date(2020,5,19) 
     },
     {
       id: 4,
       title: 'Lambergini',
       amount: 888889,
-      date: new Date(2020,5,18) 
+      date: new Date(2020,5,19) 
     },
   ];
   return (
